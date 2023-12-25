00b1f6558f4cb9958d980949e186eec9546629e9
MohsniAlaaEddine <mohsnialaaeddine@gmail.com>
2023-12-25 09:45:27 +0100

    commit

--- MERNStack/week_1/Day_5/Practice/tabs/src/App.jsx
+++ MERNStack/week_1/Day_5/Practice/tabs/src/App.jsx
@@ -1,19 +1,27 @@
-import Display from './components/Display'
-import Button from './components/Button'
 import { useState} from 'react'
 
 function App() {
-  const [buttons, setButton] = useState([
+  const buttons = [
     { name: "Tab 1", content: 'Tab 1 content is showing here' }, 
     { name: "Tab 2", content:'Tab 2 content is showing here' },
-    { name: "Tab 3", content:'Tab 3 content is showing here' }])
+    { name: "Tab 3", content:'Tab 3 content is showing here' }]
 
+    const [pressedButtonContent,setPressedButtonContent]= useState(null);
   return (
+    <>
     <fieldset>
-      <legend>Tabs</legend>
-      <Button  buttons={buttons}/>
-      <Display buttons={buttons}/>
+        <legend>Tabs</legend>
+        <fieldset>
+      <legend>Button</legend>
+      {buttons.map((button, idx) => <button onClick ={()=>{setPressedButtonContent(button.content)}} key={idx}>{button.name}</button>)}
+
     </fieldset>
+    <fieldset>
+      <legend> Hello </legend>
+      <p> {pressedButtonContent}</p>
+    </fieldset>
+      </fieldset>
+      </>
   ) 
 }
 
