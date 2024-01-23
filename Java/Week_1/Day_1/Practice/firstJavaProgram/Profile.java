14859cecac5fbe802713dcd927213f1db954f590
MohsniAlaaEddine <mohsnialaaeddine@gmail.com>
2024-01-23 14:31:31 +0100

    commit

---
+++ Java/Week_1/Day_1/Practice/firstJavaProgram/Profile.java
@@ -0,0 +1,8 @@
+public class Profile{
+    public static void main(String[] args){
+        System.out.println("My name is Alaa Mohsni");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Manouba");
+
+    }
+}
